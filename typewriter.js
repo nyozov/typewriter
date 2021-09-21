ee68efb314206e6a936cdcf284e55281f4721e2c
Nick Yozov <n_yozov@hotmail.com>
2021-09-21 18:39:35 +0000

    typewriter

---
+++ typewriter.js
@@ -0,0 +1,11 @@
+const sentence = "hello there from lighthouse labs";
+let i = 0;
+for (const char of sentence){
+  setTimeout(()=>{
+    
+    process.stdout.write(char)
+  }, i);
+  i +=50;
+ 
+}
+setTimeout(() => process.stdout.write('\n'), i)
